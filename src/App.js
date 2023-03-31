9f536e83da28eaaa0dd7e43eb2ed8ddc2834c662
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-31 16:09:52 +0200

    d5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
+import MyFooter from './Components/MyFooter';
 function App() {
   return (
     <>
     <Header/>
+    <MyFooter/>
     </>
   );
 }
