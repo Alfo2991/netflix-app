99414c6ddebbc32f330b6c788a5d46a774605af0
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-31 17:00:15 +0200

    body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
+import MovieGallery from './Components/MovieGallery';
 import MyFooter from './Components/MyFooter';
 function App() {
   return (
     <>
     <Header/>
+    <MovieGallery/>
     <MyFooter/>
     </>
   );
--- src/Components/MovieGallery.jsx
+++ src/Components/MovieGallery.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import SingleMovie from "./SingleMovie";
 
-export default function MovieGallery() {
-  return (
-    <div>
-      
-    </div>
-  )
+class MovieGallery extends React.Component {
+  state = {
+    movies: [],
+  };
+  componentDidMount() {
+    this.fetchMovies();
+  }
+  fetchMovies = async () => {
+    const apiLink = "http://www.omdbapi.com/?apikey=ddb6f24";
+    let response = await fetch(`${apiLink}&s="${this.props.title.toLowerCase()}"`);
+    let moviesRequest = await response.json();
+    this.setState({ movies: moviesRequest }, () => console.log(this.state.movies.Search));
+  };
+  render() {
+    return (
+      <>
+        <SingleMovie title={this.props.category} movies={this.state.movies.Search} />
+      </>
+    );
+  }
 }
+
+export default MovieGallery;
